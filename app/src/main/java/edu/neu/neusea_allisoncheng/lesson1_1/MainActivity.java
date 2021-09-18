066db7f5b8341435527cc342f72bb48e7d636c61
mucheng <cheng.mu@northeastern.edu>
2021-09-17 21:01:46 -0700

    Hello World app HM1.1 code

---
+++ app/src/main/java/edu/neu/neusea_allisoncheng/lesson1_1/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.neu.neusea_allisoncheng.lesson1_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
+}
